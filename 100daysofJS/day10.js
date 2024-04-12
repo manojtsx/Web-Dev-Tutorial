e3fbb39760c8c8577c4a60c9b1c441e687081b5b
Manoj Shrestha <shresthanewar678@gmail.com>
2024-04-12 08:40:34 +0545

    100daysofJS day11

--- 100daysofJS/day10.js
+++ 100daysofJS/day10.js
@@ -6,10 +6,20 @@ console.log(arraysAreEqual([1,2,3],[1,2,3]))  Output : true
 console.log(arraysAreEqual([1,2,3],[1,2,4]))  Output : false
 console.log(arraysAreEqual([],[]))  Output : true
 
-
 Note: 
 1. The function should return false if the arrays have different lengths.
 2. The function should compare each element of arr1 with the corresponding elememnt in arr2.
 3. The function should return true only if all elements in arr1 are equal to their corresponding elements in arr2.
 
 */
+
+function arraysAreEqual(arr1, arr2){
+    for(let i = 0 ; i <= arr1.length ; i++){
+        if(arr1[i] != arr2[i]){
+            return false;
+        }
+    }
+    return true;
+}   
+const result = arraysAreEqual([1,2,3],[1,2,4]);
+console.log(result); //Output: false
---
+++ 100daysofJS/day11.js
@@ -0,0 +1,41 @@
+/*
+Q. Write a function that takes a number as input and returns the sum of its digits.
+
+//Example usage:
+console.log(sumOfDigits(1234)) Output: 10
+console.log(sumOfDigits(4321)) Output: 10
+console.log(sumOfDigits(123456)) Output: 21
+
+Constraints: 
+1. The input number will always be a positive integer.
+2. The input can have multiple digits.
+3. The output should be thes um of all the digits in the input number.
+
+Homework: 
+1. This function calculates the sum of digits without converting the number to  a string.
+
+*/
+// BY CONVERTING INTO STRING
+function sumOfDigits(num){
+    let splittedNum = num.toString().split("");
+    let sum = 0;
+    for(let number of splittedNum){
+        sum += Number(number);
+    }
+    return sum;
+}
+const result = sumOfDigits(123456);
+console.log(result);
+
+// WITHOUT CONVERTING INTO STRING
+function sumOfDigits(num){
+    let sum = 0;
+    while(num > 0){
+        let digit = num % 10; // GET THE LAST DIGIT AS IT IS THE REMAINDER
+        sum += digit; //ADD THE REMAINDER TO THE SUM
+        num = Math.floor(num / 10); // REMOVE THE LAST DIGIT
+    }
+    return sum;
+}
+const result1 = sumOfDigits(123456);
+console.log(result1);
