db57172d2cd85fcbb1144e340138157b07c4ed1b
Manoj Shrestha <shresthanewar678@gmail.com>
2024-04-12 23:35:45 +0545

    100daysofJS day12

--- 100daysofJS/day12.js
+++ 100daysofJS/day12.js
@@ -3,7 +3,7 @@ Q. Write a function that takes an array of integers as input and removes any dup
 
 Example: 
 console.log(removeDuplicates([1,2,3,2,1,4])) //Output: [1,2,3,4]
-console.log(removeDuplicates([5,6,7,7,8,8,9])) //Output: [5,6,7,8,89]
+console.log(removeDuplicates([5,6,7,7,8,8,9])) //Output: [5,6,7,8,9]
 console.log(removeDuplicates([1,2,3,4])) //Output: [1,2,3,4]
 console.log(removeDuplicates([])) //Output: []
 
@@ -16,3 +16,21 @@ Constraints:
 4. The output array should retain the original order of elements from the input array.
 
 */
+function removeDuplicates(arr){
+    let newArray = [];
+    for(let num of arr){
+        if(!newArray.includes(num)){
+            newArray.push(num);
+        }
+    }
+    return newArray;
+}
+const result = removeDuplicates([1,2,3,2,1,4]);
+console.log(result); //Output: [1,2,3,4]
+
+function removeDuplicates(arr){
+    let newArray = new Set(arr);
+    return Array.from(newArray);
+}
+const result1 = removeDuplicates([1,2,3,2,1,4]);
+console.log(result1);
