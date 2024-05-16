8d72c72e8745a1a45933f6cb72be4bde13c3fdc4
Manoj Shrestha <shresthanewar678@gmail.com>
2024-05-16 18:51:33 +0545

    added insertion of doubly linked list

--- challenge chatgpt/challenge13.js
+++ challenge chatgpt/challenge13.js
@@ -64,6 +64,62 @@ var DoublyLinkedList = /** @class */ (function () {
             current = current.next;
         }
     };
+    // Insert at beginning
+    DoublyLinkedList.prototype.insertAtBeginning = function (data) {
+        var newNode = new DoublyLinkedListNode(data);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+    };
+    // Insert at End 
+    DoublyLinkedList.prototype.insertAtEnd = function (data) {
+        var newNode = new DoublyLinkedListNode(data);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            newNode.prev = this.tail;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    };
+    // Insert at Position
+    DoublyLinkedList.prototype.insertAtPosition = function (data, pos) {
+        var newNode = new DoublyLinkedListNode(data);
+        if (this.head === null) {
+            console.log("List is empty. So, insert at first");
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            var current = this.head;
+            var i = 1;
+            while (i < pos && current.next !== null) {
+                current = current.next;
+                i++;
+            }
+            if (i < pos) {
+                console.log("Position out of range");
+                return;
+            }
+            newNode.next = current.next;
+            if (current.next !== null) {
+                current.next.prev = newNode;
+            }
+            newNode.prev = current;
+            current.next = newNode;
+            if (newNode.next === null) {
+                this.tail = newNode;
+            }
+        }
+    };
     return DoublyLinkedList;
 }());
 var list = new DoublyLinkedList();
@@ -73,3 +129,12 @@ list.insert(3);
 list.display(); // Output: 1 2 3
 list.delete(2);
 list.display(); // Output: 1 3
+list.insertAtBeginning(0);
+list.display(); // Output: 0 1 3
+list.insertAtEnd(4);
+list.display(); // Output: 0 1 3 4
+list.insertAtPosition(2, 2);
+list.display(); // Output: 0 1 2 3 4
+list.insertAtPosition(5, 5);
+list.display(); // Output: 0 1 2 3 4 5
+list.insertAtPosition(6, 7); // Output: Position out of range
--- challenge chatgpt/challenge13.ts
+++ challenge chatgpt/challenge13.ts
@@ -66,6 +66,62 @@ class DoublyLinkedList<T> {
       current = current.next;
     }
   }
+
+  // Insert at beginning
+  insertAtBeginning(data : T) : void{
+    let newNode = new DoublyLinkedListNode<T>(data);
+    if(this.head === null){
+      this.head = newNode; 
+      this.tail = newNode;
+    }else{
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+  }
+
+  // Insert at End 
+  insertAtEnd(data : T) : void {
+    let newNode = new DoublyLinkedListNode<T>(data);
+    if(this.head === null){
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      newNode.prev = this.tail;
+      this.tail!.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  // Insert at Position
+  insertAtPosition(data : T, pos : number) : void{
+    let newNode = new DoublyLinkedListNode<T>(data);
+    if(this.head === null){
+      console.log("List is empty. So, insert at first");
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      let current = this.head;
+      let i = 1;
+      while(i < pos && current.next !== null){
+        current = current.next;
+        i++;
+      }
+      if(i < pos){
+        console.log("Position out of range");
+        return;
+      }
+      newNode.next = current.next;
+      if(current.next !== null){
+        current.next.prev = newNode;
+      }
+      newNode.prev = current;
+      current.next = newNode;
+      if(newNode.next === null){
+        this.tail = newNode;
+      }
+    }
+  }
 }
 
 let list = new DoublyLinkedList<number>();
@@ -75,3 +131,12 @@ list.insert(3);
 list.display(); // Output: 1 2 3
 list.delete(2);
 list.display(); // Output: 1 3
+list.insertAtBeginning(0);
+list.display(); // Output: 0 1 3
+list.insertAtEnd(4);
+list.display(); // Output: 0 1 3 4
+list.insertAtPosition(2, 2);
+list.display(); // Output: 0 1 2 3 4
+list.insertAtPosition(5, 5);
+list.display(); // Output: 0 1 2 3 4 5
+list.insertAtPosition(6, 7); // Output: Position out of range
